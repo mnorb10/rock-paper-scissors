d7c3b014ad5c7789d61244d6c467c3c00fd306e5
Matthew Norberg <norbergmatthew@gmail.com>
2021-04-17 12:19:19 -0400

    modify script file

--- scripts/script.js
+++ scripts/script.js
@@ -1,35 +1,81 @@
-/* javascript*/
-function playerPlay() 
-{let randomNumber = (Math.floor(Math.random() * 3)); 
-    if (randomNumber==0){playerMove = "Rock";} 
-    else if (randomNumber==1) {playerMove = "Paper";} 
-    else {playerMove="Scissors";}
-    return(playerMove)
-}
+  /* javascript*/
+  /*Player Move, uses random number and assigns "Rock", "Paper", or "Scissors"*/  
 
-/*Computer Random Move (rock, paper, or scissors)*/
-function computerPlay() 
-{let randomNumber = (Math.floor(Math.random() * 3)); 
-    if (randomNumber==0){computerMove = "Rock";} 
-    else if (randomNumber==1) {computerMove = "Paper";} 
-    else {computerMove="Scissors";}
-    return(computerMove)
-}
+  function playerPlay() 
+  {   
+      let numberGeneratedPlayer=(Math.floor(Math.random() * 3));
+      if (numberGeneratedPlayer===0) {
+              return("Rock");
+        } 
+      else if (numberGeneratedPlayer===1) {
+              return("Paper");
+        } 
+      else {
+              return("Scissors")
+        }
+  };
+ 
+  /*Computer Move, uses random number and assigns "Rock", "Paper", or "Scissors"*/
 
-function playRound(playerSelection,computerSelection)
-{
-    if (computerSelection==playerSelection) {winner=alert("draw");}
-    else if ((computerSelection =="Rock") && (playerSelection=="Paper")) {winner=alert("You Win!");}
-    else if ((computerSelection =="Rock") && (playerSelection=="Scissors")) {winner=alert("You lose!");}
-    else if ((computerSelection =="Paper") && (playerSelection=="Rock")) {winner=alert("You lose!");}
-    else if ((computerSelection =="Paper") && (playerSelection=="Scissors")) {winner=alert("You Win!");}
-    else if ((computerSelection =="Scissors") && (playerSelection=="Rock")) {winner=alert("You Win!");}
-    else if ((computerSelection =="Scissors") && (playerSelection=="Paper")) {winner=alert("You lose!");}
-    else {winner=alert("you win");}
-    return(winner)
-}
-const playerSelection = playerPlay()
-const computerSelection = computerPlay()
+  function computerPlay()   
+  {   
+      let numberGeneratedComputer=(Math.floor(Math.random() * 3)); 
+      if (numberGeneratedComputer===0) {
+              return("Rock");
+        } 
+      else if (numberGeneratedComputer===1) {
+              return("Paper");
+        } 
+      else {
+              return("Scissors")
+        }
+  }; 
 
-console.log(playRound(playerSelection,computerSelection),[playerSelection,computerSelection])
+                   
+/*Play one round of rock, paper, scissors*/
+  function playRound() {
+      const computer = computerPlay();
+      const player = playerPlay();
+      let outcome;
+      if(computer ==="Rock" && player ==="Rock") {
+          outcome="Draw! Pick Again";
+        }
+      else if(computer ==="Paper" && player ==="Paper"){
+              outcome="Draw! Pick Again";
+        }
+      else if (computer ==="Scissors" && player ==="Scissors"){
+              outcome="Draw! Pick Again";
+        }   
+      else if (computer ==="Rock" && player ==="Paper") {
+              outcome="You Win! Paper Beats Rock";
+        }
+      else if (computer ==="Rock" && player ==="Scissors") {
+              outcome="You Lose! Rock Beats Scissors";
+        }
+      else if (computer ==="Paper" && player ==="Rock") {
+              outcome="You Lose! Paper Beats Rock";
+        }
+      else if (computer ==="Paper" && player ==="Scissors") {
+              outcome="You Win! Scissors Beats Paper";
+        }   
+      else if (computer ==="Scissors" && player ==="Rock") {
+              outcome="You Win! Rock Beats Scissors";
+        }
+      else if (computer ==="Scissors" && player ==="Paper") { 
+              outcome="You Lose! Scissors Beats Paper";
+        }      
+         else {
+                 outcome="i dont know what happened";
+                }   
+         return outcome;
+  };  
+    
+ /* play five rounds of rock paper scissors*/
+  
+  function game()      
+  {
+      return[playRound(),playRound(),playRound(),playRound(),playRound()]
+
+  };  
+  console.log(game())  
 
